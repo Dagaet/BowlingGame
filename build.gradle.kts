4b9f525aea672fe7ad2095889fc2d2ed195d5758
Saúl Arteaga <31862646+Dagaet@users.noreply.github.com>
2024-05-23 09:12:28 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("com.approvaltests:approvaltests:23.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
